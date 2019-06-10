2c78f74882f2120ca171ef45c1bac954b8994a0d
fortandh <2217496259@qq.com>
2019-06-11 07:37:43 +0800

    # Note Finished the bug about the lack of usage of the select() function fixed.

--- ga.py
+++ ga.py
@@ -96,8 +96,8 @@ if __name__ == "__main__":
         new_path_list = []
         for j in range(selection_size):
             for k in range(j+1, selection_size):
-                new_path1 = alg.cross_over(paths[j].points, paths[k].points, objectives, Kca)
-                new_path2 = alg.cross_over(paths[k].points, paths[j].points, objectives, Kca)
+                new_path1 = alg.cross_over(selected_path_list[i].points, selected_path_list[k].points, objectives, Kca)
+                new_path2 = alg.cross_over(selected_path_list[k].points, selected_path_list[j].points, objectives, Kca)
 
                 new_path1_mutated = alg.mutate(new_path1, objectives, Kca)
                 new_path1_mutated.fitness = alg.get_fitness(new_path1_mutated, occ_grid, pri_grid,
